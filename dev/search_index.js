d105fa986a0030f5e777ae1a6d657ad738f89599
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-02-08 22:56:56 +0000

    build based on 96bfad3

---
+++ dev/search_index.js
@@ -0,0 +1,51 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "intro/#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "intro/#Turing-versions-of-TuringModels-models-1",
+    "page": "Home",
+    "title": "Turing versions of TuringModels models",
+    "category": "section",
+    "text": "This package contains Julia versions of  mcmc models contained in the R package \"rethinking\" associated with the book Statistical Rethinking by Richard McElreath implemented using Turing.jl."
+},
+
+{
+    "location": "#",
+    "page": "Functions",
+    "title": "Functions",
+    "category": "page",
+    "text": "CurrentModule = TuringModels"
+},
+
+{
+    "location": "#TuringModels.rel_path_t-Tuple",
+    "page": "Functions",
+    "title": "TuringModels.rel_path_t",
+    "category": "method",
+    "text": "relpatht\n\nRelative path using the TuringModels src/ directory.\n\nExample to get access to the data subdirectory\n\nrel_path_t(\"..\", \"data\")\n\n\n\n\n\n"
+},
+
+{
+    "location": "#rel_path_t-1",
+    "page": "Functions",
+    "title": "rel_path_t",
+    "category": "section",
+    "text": "rel_path_t(parts...)"
+},
+
+{
+    "location": "#generate-1",
+    "page": "Functions",
+    "title": "generate",
+    "category": "section",
+    "text": "generate_t_(; sd=script_dict_t)\ngenerate_t(chapter::AbstractString; sd=script_dict_t_)\ngenerate_t(chapter::AbstractString, scriptfile::AbstractString; sd=script_dict_t_)"
+},
+
+]}
